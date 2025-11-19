6e940f6f1506f86109739995d27ffba0ac2a336c
nourhesham116 <nourhesham116@gmail.com>
2025-11-19 11:43:31 +0200

    init: tasks_app with DRF, clean and modular structure

--- onboarding/settings.py
+++ onboarding/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'rest_framework',   # for Django REST Framework
+    'tasks_app',        # our new app
 ]
 
 MIDDLEWARE = [
